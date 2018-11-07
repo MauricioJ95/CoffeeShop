96c6a1de77c483f721dc666549b7e8f9809b1b20
Bourke, David <davidbourke@quickenloans.com>
2018-11-07 18:24:06 -0500

    Added email validation

---
+++ CoffeeShop/Scripts/register.js
@@ -0,0 +1,17 @@
+﻿function isValidEmail(email) {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        return (true);
+    }
+    return (false);
+}
+
+function onFormSubmit() {
+    var emailAddress = document.getElementById('email').value;
+
+    if (!isValidEmail(emailAddress)) {
+        alert("You have entered an invalid email address!");
+        return false;
+    }
+
+    return true;
+}
