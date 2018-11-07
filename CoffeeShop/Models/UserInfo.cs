40a5281cd4d3f670e7d3be0b9e58ab440f96ae69
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-11-07 16:11:35 -0500

    example push

--- CoffeeShop/Models/UserInfo.cs
+++ CoffeeShop/Models/UserInfo.cs
@@ -25,7 +25,7 @@ namespace CoffeeShop.Models
         public string PhoneNumber { set; get; }
 
         [Required]
-        [RegularExpression(@"^[A-z0-9]{2,}$")]
+        [RegularExpression(@"^[A-z0-9]{8,}$")]
         public string Password { set; get; }
         public UserInfo()
         {
